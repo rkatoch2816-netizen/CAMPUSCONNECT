cd1f2430222afc8b54f381ee2ccb7dc448f7249d
Poonam Jaryal <128497368+pj642@users.noreply.github.com>
2026-03-02 20:56:09 +0530

    Create OfficeDashboardActivity.java

---
+++ app/src/main/java/com/campusconnect/officeDashboard/OfficeDashboardActivity.java
@@ -0,0 +1,12 @@
+package com.campusconnect.officeDashboard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OfficeDashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office_dashboard);
+    }
+}
